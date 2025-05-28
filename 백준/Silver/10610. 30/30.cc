9ba116c1621562cc1615a992866d74d731c4c742
Jeeyoon Lee <85072967+Jeeyoon038@users.noreply.github.com>
2025-05-28 19:46:13 +0900

    Update 30.cc

--- 백준/Silver/10610. 30/30.cc
+++ 백준/Silver/10610. 30/30.cc
@@ -14,21 +14,20 @@ int main() {
 
 	for (char c : num) {
 		digits.push_back(c - '0');
-	}
+	}//string num의 문자를 숫자로 바꿔서 vector에 저장
 
 	int length = digits.size();
 	int sum = 0;
 	bool hasZero = false;
 
 	for (int i = 0;i < length;i++) {
-		
-		sum = sum + digits[i];
-		if (digits[i] == 0) hasZero = true;
-	}
 
+		sum = sum + digits[i]; //숫자의 각 자리 수를 합한 sum
+		if (digits[i] == 0) hasZero = true; //숫자에 0이 포함되어있는지 확인
+	}
 	
 	if (sum % 3 == 0 and hasZero!=0) {
-		sort(digits.begin(), digits.end(), greater<int>());
+		sort(digits.begin(), digits.end(), greater<int>()); //배열을 내림차순으로 정렬
 		for (int num : digits) {
 			cout << num;
 		}
