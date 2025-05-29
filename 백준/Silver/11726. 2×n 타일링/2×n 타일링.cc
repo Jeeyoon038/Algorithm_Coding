5a35991f5ece5f7802c1b371148da75ab5338942
Jeeyoon Lee <85072967+Jeeyoon038@users.noreply.github.com>
2025-05-29 23:58:10 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    int dp[1001];
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= num; i++) {
+        dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+    }
+
+    cout << dp[num];
+    return 0;
+}
