5d802ef9ed62969e71270bcd377a24092a0f09f3
Jeeyoon Lee <85072967+Jeeyoon038@users.noreply.github.com>
2025-05-29 20:07:32 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 104 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+
+	vector <pair<int,int>> location;
+
+	for (int i = 0;i < num;i++) {
+		int x, y;
+		cin >> x >> y;
+		location.push_back({x,y});
+	}
+
+	sort(location.begin(), location.end(), [](pair<int, int>a, pair<int, int>b) {//람다 함수
+		if (a.second == b.second) return a.first < b.first;
+		return a.second < b.second; //y좌표 기준 정렬
+		});
+
+
+
+	for (auto a : location) { //auto는 타입을 자동으로 추론하는 키워드
+		cout << a.first << " " << a.second << "\n"; //pair의 first와 second
+	}
+	return 0;
+}
