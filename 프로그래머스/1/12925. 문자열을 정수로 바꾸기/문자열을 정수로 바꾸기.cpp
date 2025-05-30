812d6e510300d87d65793387724e56a5377ea199
Jeeyoon Lee <85072967+Jeeyoon038@users.noreply.github.com>
2025-05-31 06:01:53 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    
+    answer=stoi(s);
+    //stoi는 문자열을 정수로 바로 변환해 준다.
+    return answer;
+}
