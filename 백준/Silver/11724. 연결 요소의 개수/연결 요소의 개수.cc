5acc8f85c7f0cb9b548da000859e02ed16a35ecf
Jeeyoon Lee <85072967+Jeeyoon038@users.noreply.github.com>
2025-06-01 15:38:11 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 288 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -1,50 +1,41 @@
 #include <iostream>
-#include <queue>
-#include <string.h>
+#include <vector>
 
 using namespace std;
-
-int arr[1001][1001]; //인접행렬
-int visited[1001]; //방문기록
 int N, M;
+int arr[1001][1001];
+int visited[1001];
 
-//DFS - stack 사용
-void DFS(int V) {
-
-	visited[V] = 1; //시작점 방문기록
-
+void DFS(int n) {
+	visited[n] = 1;
 	for (int i = 0;i <= N;i++) {
-		if (arr[V][i] == 1 && visited[i] == 0)
-		{
-			DFS(i); //스택에 i를 넣는 것과 같음
+		if (arr[n][i] == 1 && visited[i] != 1) {
+			DFS(i);
 		}
 		if (i == N)
 			return;
 	}
 }
-
-
-
 int main() {
-
-	int u, v;
 	cin >> N >> M;
 
 	for (int i = 0;i < M;i++) {
+		int u, v;
 		cin >> u >> v;
 		arr[u][v] = 1;
 		arr[v][u] = 1;
 	}
 
-	int cnt = 0;//연결 요소 개수
+	int cnt = 0;
 
 	for (int i = 1;i <= N;i++) {
-		if (visited[i] == 0) {
+		if (visited[i] != 1) {
 			DFS(i);
 			cnt++;
 		}
 	}
-	
-	cout << cnt << endl;
+
+	cout << cnt;
+
 	return 0;
 }
